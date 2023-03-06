8bee1c2b2e4ba9da4620b4a0563e2dacf4a53cde
Asfiya <peerzade1294@gmail.com>
2023-03-06 12:46:04 +0530

    initial project

---
+++ nodeServer/index.js
@@ -0,0 +1,22 @@
+//Node server that will handle socket.io connections
+
+const io = require('socket.io')(8000)
+const users ={};
+
+  io.on('connection', socket=>{
+
+    socket.on('new-user-joined', name =>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send',message =>{
+        broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', name =>{
+      socket.broadcast.emit('left', users[socket.id]);
+      delete users[socket.io];
+    });
+    
+  })
